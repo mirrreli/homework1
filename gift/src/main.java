03c45385b722a8e3b2cf70b0c49c845521ca3963
mirrreli <100945503+mirrreli@users.noreply.github.com>
2022-07-18 21:53:09 +0300

    new gift

--- gift/src/main.java
+++ gift/src/main.java
@@ -3,33 +3,24 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         int count = 0;
-        int i,j,k;
         Scanner in = new Scanner(System.in);
         System.out.print("Enter weights: ");
         int gift = in.nextInt();
         int apple = in.nextInt();
         int orange = in.nextInt();
         int pear = in.nextInt();
+        int applenum = gift / apple;
+        int orangenum = gift / orange;
+        int sum, sum1, sum2, sum3 = 0;
 
-        int[] arr1 = {apple, orange, pear};
-        for(int n = arr1.length-1 ; n > 0 ; n--) {
-            for (int m = 0; m < n; m++) {
-                if (arr1[m] > arr1[m + 1]) {
-                    int tmp = arr1[m];
-                    arr1[m] = arr1[m + 1];
-                    arr1[m + 1] = tmp;
-                }
-            }
-        }
-        int maxnum = gift/arr1[0];
-        int middlenum = gift/arr1[1];
-        int minnum = gift/arr1[2];
-
-        for(i = 0;  i <= maxnum; i++){
-            for(j = 0;  j <= middlenum; j++){
-                for(k = 0;  k <= minnum; k++){
-                    if(gift == (i*arr1[0] + j*arr1[1] + k*arr1[2])){
-                        count+=1;
+        for (int i = 0; i <= applenum; i++) {
+            for (int j = 0; j <= orangenum; j++) {
+                sum = apple * i;
+                sum1 = orange * j;
+                sum2 = (gift - sum - sum1) / pear;
+                if (sum2 >= 0) {
+                    if (sum2 * pear + sum1 + sum == gift) {
+                        count++;
                     }
                 }
             }
